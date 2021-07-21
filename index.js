c0e8fae79064f4aad15b27c239ab37f8ac393120
ChristianSheherlis <57498743+ChristianSheherlis@users.noreply.github.com>
2021-07-21 10:42:27 -0400

    LESSON 3

---
+++ index.js
@@ -0,0 +1,8 @@
+//Strings
+const str = "Hello, my name is";
+const name = "Mike";
+const greet = "How are you today?";
+const newStr = str + name + greet;
+//template literals
+const literal = `This is a new string, is it not cool? ${str} ${name} ${greet}`;
+console.log(literal);
